206c6770df5a9b509045144c397f42b35143f2cc
gangsanlee <116553043+gangsanlee2@users.noreply.github.com>
2023-01-11 09:34:26 +0900

    01.11.09:34

--- fastApiServer/app/routers/user.py
+++ fastApiServer/app/routers/user.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 import app.repositories.user as dao
 from app.database import get_db
-from app.models.user import User
+from app.schemas.user import User
 
 router = APIRouter()
 
